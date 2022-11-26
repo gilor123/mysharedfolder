96fb2fcd5869c10e9be41e7b466697dc3d4986ee
gilor123 <63399696+gilor123@users.noreply.github.com>
2022-11-26 23:39:44 +0200

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import os
 
 from threading import Thread
 from state import SharedFolderState
-w
+
 base_path = "C:/Users/User/Downloads/networking_project/Server_Folder"
 
 # Create base path if doesn't exist
@@ -13,14 +13,14 @@ try:
 except:
     pass
 
-state = SharedFolderState.from_real_dir(base_path)
-state.version = 0
-
+state_obj = SharedFolderState.from_real_dir(base_path,None)
+state_obj.version = 0
+# state_obj.print_state() @TODO REMOVE AFTER TESTINGS
 clients = []
 
 
 def handle_client(client_socket):
-    global state
+    global state_obj
     unpacker = msgpack.Unpacker(use_list=False, raw=False)
 
     while True:
@@ -34,19 +34,20 @@ def handle_client(client_socket):
             # Client just sent a new state. Load it.
             client_state = SharedFolderState.from_dict(unpacked)
 
-            if client_state.version == state.version:
-                state = client_state
-                state.version += 1
-                state.write(base_path)
+            if client_state.version == state_obj.version:
+                state_obj = client_state
+                state_obj.version += 1
+                state_obj.write_state_to_folder(base_path)
+                print("[SERVER] Received new state from client, new version", state_obj.version)
 
                 # Update all clients with the new state.
-                serialized_state = msgpack.dumps(state.to_dict())
+                serialized_state = msgpack.dumps(state_obj.to_dict())
                 for client in clients:
                     client.send(serialized_state)
-
-                print("Received new state from client, new version", state.version) 
+            # If receive an old version, need to updates the client with the current version
             else:
-                client_socket.send(msgpack.dumps(state.to_dict()))
+                print("[SERVER] a client isn't up to date!")
+                client_socket.send(msgpack.dumps(state_obj.to_dict()))
 
 
 def main():
@@ -54,18 +55,16 @@ def main():
     server = socket.socket()
     server.bind(("0.0.0.0", 5055))
     server.listen()
-    print ("I'm listening")
+    print(f"[SERVER] Listening to TBD PORT and ADDRESS")
 
     while True:
         client_socket, _ = server.accept()
-        client_socket.send(msgpack.dumps(state.to_dict()))
+        client_socket.send(msgpack.dumps(state_obj.to_dict()))
 
         clients.append(client_socket)
 
         thread = Thread(target=handle_client, args=(client_socket, ))
         thread.start()
-
-
 
 
 
