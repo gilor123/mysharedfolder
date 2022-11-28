99bb0a68bcdbde2e5e4026ac3a0368dd1ad8e9f3
gilor123 <63399696+gilor123@users.noreply.github.com>
2022-11-28 22:31:07 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,19 @@
-import threading
+# Client.py: Create client and file observer, and handle server events
+
 from threading import Thread
 import time
 import os
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
 import msgpack
 import socket
-import sys
 from state import SharedFolderState
 import shutil
+from observer import  MyFileSystemEventHandler
 
 state_obj = None
+
+PORT = 5055
+ADDRESS = socket.gethostbyname ( socket.gethostname () )
 
 
 # Cleans the content of the directory
@@ -25,40 +28,57 @@ def clean_directory(base_path):
     except :
         pass
 
+
+# Create a file system observer
+def create_observer (base_path, client_socket):
+    global state_obj
+    event_handler = MyFileSystemEventHandler( base_path, client_socket, state_obj )
+    observer = Observer( )
+    observer.schedule( event_handler, base_path, recursive = True )
+    observer.start( )
+
+    try :
+        while True :
+            time.sleep( 1 )
+    except KeyboardInterrupt :
+        observer.stop( )
+
+    observer.join( )
+
+
 # Monitor the directory and send it updated state to the server
 def watch_directory (base_path, client_socket):
     global state_obj
-    while True:
-        state_obj = SharedFolderState.from_real_dir(base_path,state_obj)
-        # state_obj.print_state()@TODO REMOVE AFTER TESTING
-        print("[CLIENT] Sending state to server")
-        client_socket.send( msgpack.dumps( state_obj.to_dict( ) ) )
-        time.sleep(3)
+
+    thread_observer = Thread(target=create_observer, args=(base_path, client_socket))
+    thread_observer.start()
 
 
 def main ( ) :
     global state_obj
     global event
 
-    # Read base path from the command line. @TODO CHANGE TO BE AN INPUT IN THE END
+    # Read base path from the command line.
     print("Please enter the full path to your local shared folder:")
     base_path = input()
-    # base_path = "C:/Users/User/Downloads/networking_project/Client_Folder_1"
 
     # Clean directory
     clean_directory(base_path)
 
     # Connect to server
     client_socket = socket.socket( )
-    client_socket.connect( ("127.0.0.1", 5055) )
-    print(f"[CLIENT] Listening to TBD PORT and ADDRESS")
+    client_socket.connect( (ADDRESS, PORT) )
+    print(f"[CLIENT] Listening to port {PORT}")
 
-    # # Start observing the base path directory.
-    thread_observer = Thread(target=watch_directory, args=(base_path, client_socket))
-    thread_observer.start()
 
     # Listen to messages from the server
     unpacker = msgpack.Unpacker( use_list = False, raw = False )
+
+    # Start observing
+    event_handler = MyFileSystemEventHandler( base_path, client_socket, state_obj )
+    observer = Observer( )
+    observer.schedule( event_handler, base_path, recursive = True )
+
     while True :
         # Receive the data
         data = client_socket.recv( 1024 )
@@ -66,12 +86,19 @@ def main ( ) :
             break
         unpacker.feed( data )
         for unpacked in unpacker :
+            # We just received our first state, start observing.
+            observer.stop()
+
             # Update state
             state_obj = SharedFolderState.from_dict( unpacked )
             state_obj.write_state_to_folder( base_path )
-
             print( f"[CLIENT] The real folder was updated based on state version {state_obj.version}" )
 
+            # Restart observer
+            event_handler = MyFileSystemEventHandler( base_path, client_socket, state_obj )
+            observer = Observer()
+            observer.schedule(event_handler, base_path, recursive=True)
+            observer.start()
 
 
 if __name__ == "__main__" :
